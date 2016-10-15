aacb373c74185560618ceade19149c64fdb9b0fc
Cedric Kisema <kisema@gmail.com>
2016-10-15 23:23:36 +0100

    fix: retrieve email and question from html

--- popup.js
+++ popup.js
@@ -2,8 +2,6 @@ document.addEventListener('DOMContentLoaded', function() {
   var address = "http://localhost:4567/api/v1/facts";
   var random_id = (Math.floor(Math.random() * (1000 - 1 + 1)) + 1).toString();
   var selectedText = "";
-  var user_email = "";
-  var user_question = "";
 
   chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
@@ -13,16 +11,17 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 
   var checkPageButton = document.getElementById('checkPage');
-
   checkPageButton.addEventListener('click', function() {
-    user_question = document.getElementById('question');
-    user_email = document.getElementById('email');
 
     chrome.tabs.getSelected(null, function(tab) {
 
+      var user_question = document.getElementById('question').value;
+      var user_email = document.getElementById('user_email').value;
+
+
       var data = {
         questionable_fact_url: tab.url,
-        email: user_email,
+        user_email: user_email,
         user_question: user_question,
         questionable_fact: selectedText,
         fact_id: random_id
