e6e73253c4324637b7c562a879367066b25a11bb
Cedric Kisema <kisema@gmail.com>
2016-10-15 22:57:54 +0100

    styling and added addition fields to json post

--- popup.js
+++ popup.js
@@ -2,22 +2,29 @@ document.addEventListener('DOMContentLoaded', function() {
   var address = "http://localhost:4567/api/v1/facts";
   var random_id = (Math.floor(Math.random() * (1000 - 1 + 1)) + 1).toString();
   var selectedText = "";
+  var user_email = "";
+  var user_question = "";
 
   chrome.tabs.executeScript( {
     code: "window.getSelection().toString();"
   }, function(selection) {
     selectedText =  selection[0];
+    document.getElementById('selectedText').innerHTML = selectedText;
   });
 
   var checkPageButton = document.getElementById('checkPage');
 
   checkPageButton.addEventListener('click', function() {
+    user_question = document.getElementById('question');
+    user_email = document.getElementById('email');
 
     chrome.tabs.getSelected(null, function(tab) {
 
       var data = {
-        title: tab.url,
-        paragraph: selectedText,
+        questionable_fact_url: tab.url,
+        email: user_email,
+        user_question: user_question,
+        questionable_fact: selectedText,
         fact_id: random_id
       };
 
